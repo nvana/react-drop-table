a741453041d8021d4afe38d10642e126fbc09f58
nvana <n.vanalder@gmail.com>
2017-11-11 14:42:47 +0100

    chore(*): add less style file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Table from "./Table";
+import Table from "./components/Table";
 
 //Entry datas
 const fakeData = [
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-            presets: ['react','es2015']
-        }
-      }
-    ],
-  },
-  output: {
-    path: __dirname + "/build",
-    filename: "./index.min.js"
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ]
+	entry: "./src/index.js",
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				loader: "babel-loader",
+				exclude: /node_modules/,
+				query: {
+					presets: ["react","es2015"]
+				}
+			},
+			{ test: /\.less$/, loader: "style-loader!css-loader!less-loader" }
+		],
+
+	},
+	output: {
+		path: "/build",
+		filename: "./index.min.js"
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
 };
